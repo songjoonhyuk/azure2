eb84498c922e0521f4143a6edae7742d8bb05cb9
songjoonhyuk <songjoonhyuk@gmail.com>
2016-04-29 19:25:35 +0900

    first

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+def post_list(request):
+	post_list=Post.objects.all()
+	return render(request, 'blog/post_list.html', {'post_list' : post_list,})
+
+
+# def post_detail(request, pk):
+#  	post_detail=Post.objects.get(pk=pk)
+#  	return render(request, 'blog/post_detail.html', { 'post' : post,})
+
+def post_detail(request, pk):
+	post = Post.objects.get(pk=pk)
+	return render(request, 'blog/post_detail.html',{'post' : post,})
